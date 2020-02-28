ff84115c4857a5de33d3943e7b961082ef02427e
lhotakj <lhotakj@gmail.com>
2020-02-28 23:57:37 +0100

    gunicorn fix + heroku fix Procfile

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-gunicorn --bind 0.0.0.0:5000 wsgi:app
+export FLASK_APP=./app/app.py
+export FLASK_DEBUG=1
+export VERIFICATION_TOKEN=***DUMMY***
+export SLACK_API_TOKEN=***DUMMY****
+
+gunicorn --bind 0.0.0.0:5000 app.app:app
 
 #https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04
 #https://medium.com/faun/deploy-flask-app-with-nginx-using-gunicorn-7fda4f50066a
