0448d65731310eed148d5a66ec786b7d327b8daf
lhotakj <lhotakj@gmail.com>
2020-03-03 23:46:26 +0100

    jquery conflicts esolved

--- app/static/js/default.js
+++ app/static/js/default.js
@@ -8,7 +8,37 @@ window.chartColors = {
     grey: 'rgb(201, 203, 207)'
 };
 
-$(document).ready(function () {
+var $j = jQuery.noConflict();
+
+function auto_hide_flesh() {
+    window.setTimeout(function () {
+        $j(".alert").fadeTo(500, 0).slideUp(100, function () {
+            $j(this).remove();
+            window.auto_hide_flesh(); // in case there's more of the divs
+        });
+    }, 5000);
+}
+
+// -- called from DataTable code
+function flesh(type, text) {
+    const html = '<div class="alert alert-' + type + ' alert-dismissible fade show" role="alert" style="position:absolute;left: calc(50% + 110px);transform: translate(-50%, 0);"> <button type="button" class="close" onclick="window.auto_hide_flesh()" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + text + ' </div>';
+    $j(".main_").prepend(html);
+}
+
+function call_api(endpoint, payload, text_success) {
+    $j.ajax(endpoint, {
+        type: 'POST',  // http method
+        data: payload,  // data to submit
+        success: function (data, status, xhr) {
+            window.flesh('success', text_success)
+        },
+        error: function (jqXhr, textStatus, errorMessage) {
+            window.flesh('warning', 'Error. ' + errorMessage)
+        }
+    });
+}
+
+$j(document).ready(function () {
 
     function set_cookie(cname, cvalue, exdays = 30) {
         var d = new Date();
@@ -46,43 +76,43 @@ $(document).ready(function () {
         }
     }
 
-    var sidenav_dom = $(".sidenav");
-    var main_dom = $(".main");
+    var sidenav_dom = $j(".sidenav");
+    var main_dom = $j(".main");
     var sidenav_collapsed = get_cookie("sidenav-collapsed");
 
     function sidenav_expand() {
-        $("i#toggler").attr("class", "fas fa-toggle-on");
-        $("div.sidenav header").show()
+        $j("i#toggler").attr("class", "fas fa-toggle-on");
+        $j("div.sidenav header").show()
         sidenav_dom.removeClass("sidenav-collapsed")
         main_dom.removeClass("main-collapsed")
-        $("div.sidenav a").each(function (index) {
-            $(this).removeClass("sidenav-a-collapsed");
+        $j("div.sidenav a").each(function (index) {
+            $j(this).removeClass("sidenav-a-collapsed");
         });
-        $("div.sidenav a span").each(function (index) {
-            $(this).removeClass("sidenav-a-span-collapsed");
+        $j("div.sidenav a span").each(function (index) {
+            $j(this).removeClass("sidenav-a-span-collapsed");
         });
-        $("div.main_bottom").removeClass("sidenav-a-span-collapsed");
+        $j("div.main_bottom").removeClass("sidenav-a-span-collapsed");
     }
 
     function sidenav_collapse() {
-        $("i#toggler").attr("class", "fas fa-toggle-off");
-        $("div.sidenav header").hide()
+        $j("i#toggler").attr("class", "fas fa-toggle-off");
+        $j("div.sidenav header").hide()
         sidenav_dom.addClass("sidenav-collapsed")
         main_dom.addClass("main-collapsed")
-        $("div.sidenav a").each(function (index) {
-            $(this).addClass("sidenav-a-collapsed");
+        $j("div.sidenav a").each(function (index) {
+            $j(this).addClass("sidenav-a-collapsed");
         });
-        $("div.sidenav a span").each(function (index) {
-            $(this).addClass("sidenav-a-span-collapsed");
+        $j("div.sidenav a span").each(function (index) {
+            $j(this).addClass("sidenav-a-span-collapsed");
         });
-        $("div.main_bottom").addClass("sidenav-a-span-collapsed");
+        $j("div.main_bottom").addClass("sidenav-a-span-collapsed");
     }
 
     if (sidenav_collapsed == "1") {
         sidenav_collapse()
     }
 
-    $("i#toggler").click(function () {
+    $j("i#toggler").click(function () {
         if (sidenav_dom.hasClass('sidenav-collapsed')) {
             sidenav_expand()
             set_cookie('sidenav-collapsed', '0');
@@ -94,35 +124,5 @@ $(document).ready(function () {
     });
 
 });
-
-var $j = jQuery.noConflict();
-
-function auto_hide_flesh() {
-    window.setTimeout(function () {
-        $j(".alert").fadeTo(500, 0).slideUp(100, function () {
-            $j(this).remove();
-            window.auto_hide_flesh(); // in case there's more of the divs
-        });
-    }, 5000);
-}
-
-// -- called from DataTable code
-function flesh(type, text) {
-    const html = '<div class="alert alert-' + type + ' alert-dismissible fade show" role="alert" style="position:absolute;left: calc(50% + 110px);transform: translate(-50%, 0);"> <button type="button" class="close" onclick="window.auto_hide_flesh()" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + text + ' </div>';
-    $j(".main_").prepend(html);
-}
-
-function call_api(endpoint, payload, text_success) {
-    $j.ajax(endpoint, {
-        type: 'POST',  // http method
-        data: payload,  // data to submit
-        success: function (data, status, xhr) {
-            window.flesh('success', text_success)
-        },
-        error: function (jqXhr, textStatus, errorMessage) {
-            window.flesh('warning', 'Error. ' + errorMessage)
-        }
-    });
-}
 
 window.auto_hide_flesh();
